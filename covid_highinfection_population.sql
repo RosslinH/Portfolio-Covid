1fbb75f3c3711cd96a3ea71f575a280c1ee04709
Yingwen <124633479+RosslinH@users.noreply.github.com>
2023-02-06 23:06:22 +0100

    Update covid_highinfection_population.sql

--- covid_highinfection_population.sql
+++ covid_highinfection_population.sql
@@ -1,10 +1,10 @@
 -- Looking at countries with the highest infection rate of covid compared to population
 
 SELECT 
-			location,
-			population,
-			MAX(total_cases) AS highest_infection,
-			MAX((total_cases / population)*100) AS infection_rate
+	location,
+	population,
+	MAX(total_cases) AS highest_infection,
+	MAX((total_cases / population)*100) AS infection_rate
             
 FROM covid_death
 GROUP BY location, population
