ba9e3d166e6936ea991c0456868b5c7bb0af993e
Yingwen <124633479+RosslinH@users.noreply.github.com>
2023-02-06 23:05:19 +0100

    Update covid_death_china.sql

--- covid_death_china.sql
+++ covid_death_china.sql
@@ -1,11 +1,11 @@
 -- Looking at the death rate of covid in China
 
 SELECT 
-			location,
-			date,
-			total_cases,
-			total_deaths,
-			(total_deaths / total_cases)*100 AS dealth_rate
+	location,
+	date,
+	total_cases,
+	total_deaths,
+	(total_deaths / total_cases)*100 AS dealth_rate
 
 FROM covid_death
 WHERE location LIKE 'china'
